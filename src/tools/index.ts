343bb4064444bc430219b1040d0bba2296b02da9
my-name-is-yu <yuyoshimuta1217@gmail.com>
2026-04-04 20:54:54 +0900

    feat: add tools public API index (Stage 2-B)

---
+++ src/tools/index.ts
@@ -0,0 +1,9 @@
+export { ToolRegistry } from "./registry.js";
+export type { ContextFilter, AssembledPool } from "./registry.js";
+export { ToolExecutor } from "./executor.js";
+export type { ToolExecutorDeps } from "./executor.js";
+export { ToolPermissionManager } from "./permission.js";
+export type { PermissionManagerDeps, PermissionRule } from "./permission.js";
+export { ConcurrencyController } from "./concurrency.js";
+export { createBuiltinTools } from "./builtin/index.js";
+export * from "./types.js";
